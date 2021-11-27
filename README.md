f2cf8ad2507f8a43192ca877a3be65f8c1897fa6
Ben Green <ben.green@greencodemedia.co.uk>
2021-11-27 15:53:58 +0000

    CHORE: Write tests

--- README.md
+++ README.md
@@ -9,33 +9,36 @@ This package is currently undergoing initial development. DO NOT USE until this 
 npm install @green-code/music-track-data
 ```
 
-## Usage
+## Get Tracks
 ```javascript
-const getTracks = require('@green-code/music-track-data');
+const {getTracks} = require("@green-code/music-track-data");
 
-await getTracks('Times Like These')
+await getTracks("Times Like These")
     .then(console.log)
     .catch(console.log);
 ```
-The method returns an array of objects in the format below. Each object is data of a song that the API found.
-If the API doesn't find any lyrics, you will get an empty array.
+
+## Get Lyrics
+```javascript
+const {getLyrics} = require("@green-code/music-track-data");
+
+await getLyrics("Foo Fighters", "Times Like These")
+    .then(console.log)
+    .catch(console.log);
+```
+The method returns an object in the format below.
+
+This method call uses MusicMatch and requires an API key.
 
 ```json
 {
-  "id": 0,
-  "artist": "Foo Fighters",
-  "title": "Times Like These",
-  "preview": "http://cdn-preview-9.deezer.com/stream/c-9516e9507adaaf3f0fa2354c816adeb8-5.mp3",
-  "lyrics": "I, I'm a one way motorway\nI'm the one that drives away\nThen follows you back home\nI, I'm a street light shining\nI'm a wild light blinding bright\nBurning off alone...",
-  "album": {
-    "title": "Greatest Hits",
-    "artwork": "http://e-cdn-images.dzcdn.net/images/cover/266f01f1c7a04843d11cd08f9c07d11f/1000x1000-000000-80-0-0.jpg"
-  }
+  "explicit": false,
+  "lyrics": ""
 }
 ```
 
 
 ### Info
-* Large thanks to [lyrics.ovh](https://lyrics.ovh/) and [deezer.com](https://deezer.com/).
-* Author of the API [beningreenjam](https://github.com/beningreenjam).
+* Large thanks to [MusicMatch](https://www.musixmatch.com/) and [deezer.com](https://deezer.com/).
+* Author: [beningreenjam](https://github.com/beningreenjam).
 * Forked from [s0ftik3/lyrics-api](https://github.com/s0ftik3/lyrics-api).
--- src/data.js
+++ src/data.js
@@ -1,71 +1,83 @@
 const axios = require('axios');
 const urls = require('./urls');
 
-/**
- * Looks for the data about a song.
- * @public
- * @param {string} name A song's name.
- */
+let apiKey = process.env.MUSICMATCH_API_KEY;
 
-const getTracks = async (name) => {
-    const preResult = [];
-    const results = [];
-    await axios({
+/**
+ * Looks for lyrics of an artist's track.
+ * @public
+ * @param {string} artist The artist's name.
+ * @param {string} track The track's name.
+ */
+const getLyrics = async (artist, track) => {
+  if (!apiKey) return new Error("MusicMatch API key required");
+  if (!artist) return new Error("Artist name required");
+  if (!track) return new Error("Track name required");
+
+  return axios({
+      'method': 'get',
+      'url': `${urls.urlGet}apikey=${apiKey}&q_artist=${encodeURIComponent(artist)}&q_track=${encodeURIComponent(track)}`,
+      'headers': {
+          'content-type': 'application/json',
+          'accepts': 'application/json',
+      }
+  }).then(({data}) => {
+      const {explicit, lyrics_body} = data?.message?.body?.lyrics ?? {};
+      if (!lyrics_body) return null;
+      return {explicit: !!explicit, lyrics: lyrics_body}
+  }).catch(console.error);
+}
+
+/**
+ * Looks for the data about a track.
+ * @public
+ * @param {string} name A track's name.
+ * @param {boolean} includeLyrics If you want to include lyrics for each result. (Default to false)
+ */
+const getTracks = async (name, includeLyrics = false) => {
+    if (!name) return new Error("Track name required");
+
+    const searchResults = await axios({
         'method': 'get',
         'url': `${urls.urlSearch}${encodeURIComponent(name)}`,
         'headers': {
-            'content-type': 'application/json'
+            'content-type': 'application/json',
+            'accepts': 'application/json',
         }
-    }).then((response) => {
-        const {data} = response.data;
-        for (let i = 0; i < data.length; i++) {
-            preResult.push({
-                id: i,
-                artist: data[i].artist.name,
-                title: data[i].title,
-                preview: data[i].preview,
-                album: data[i].album,
-            })
+    }).then(response => {
+        const data = response?.data?.data ?? [];
+        return data.map(({artist, title, preview, album}) => ({
+            preview,
+            title,
+            artist: artist.name,
+            album: {
+                title: album.title,
+                artwork: album.cover_xl,
+            },
+        }));
+    }).catch(console.error);
+
+    return Promise.all((searchResults || []).map(async ({title, artist, preview, album}, index) => {
+        const track = {id: index, title, artist, preview, album}
+        if (includeLyrics) {
+            track.lyrics = await getLyrics(artist, title);
         }
-    }).catch(console.log);
 
-    for (let i = 0; i < preResult.length; i++) {
-        await axios({
-            'method': 'get',
-            'url': `${urls.urlGet}${encodeURIComponent(preResult[i].artist)}/${encodeURIComponent(preResult[i].title)}`,
-            'headers': {
-                'content-type': 'application/json'
-            }
-        }).then((response) => {
-            console.log({lyrics: response.data.lyrics});
-            const paragraphs = (response.data.lyrics?.split('\n') ?? [])
-            const lyrics = paragraphs
-                .map(line => {
-                    if (line.startsWith('Paroles de la chanson') || line.startsWith('[Thanks to ')) return '';
-                    return line;
-                })
-                .filter(line => !!line)
-                .join('\n');
-
-            if (!lyrics) {
-                return;
-            }
-
-            results.push({
-                id: i,
-                title: preResult[i].title,
-                artist: preResult[i].artist,
-                preview: preResult[i].preview,
-                lyrics,
-                album: {
-                    title: preResult[i].album.title,
-                    artwork: preResult[i].album.cover_xl,
-                },
-            })
-        }).catch(console.log);
-    }
-
-    return results;
+        return track;
+    }));
 }
 
-module.exports = getTracks;
+/**
+ * Sets MusicMatch API key.
+ * @public
+ * @param {string} key API key.
+ */
+const setApiKey = (key) => {
+    apiKey = key;
+}
+
+module.exports = {
+    getTracks,
+    getLyrics,
+    setApiKey,
+};
